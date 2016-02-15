65405f4780d02f55a658428359cd07f697e08e34
kate <kate.kuchin@gmail.com>
2016-02-14 18:01:18 -0800

    puppy land

--- app/index.js
+++ app/index.js
@@ -88,14 +88,14 @@ angular.module('app', [])
 }])
 .controller('mainCtl', ['$scope', 'apiSvc', function($scope, apiSvc) {
   console.log('in main ctl');
-  $scope.JQ = jQuery;
+  var JQ = jQuery;
+  $scope.dog = 'helen';
 
   function printMessage (status='working') {		// default params
     let message = 'ES6';					            	// let
     console.log(`${message} is ${status}`);	    // template string
   }
   printMessage();
-
 
 
 }]);
